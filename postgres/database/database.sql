e2c5b3f85e5dffe0b00565a12b055c0f833141e4
duong128 <vanduongnguyen2401@gmail.com>
2021-09-27 13:18:05 +0700

    nestjs

--- postgres/database/database.sql
+++ postgres/database/database.sql
@@ -1,21 +1,52 @@
 CREATE DATABASE EnglishEducation
 
 CREATE TABLE Users(
-  id INT PRIMARY KEY,
+  user_id INT PRIMARY KEY,
   user_name VARCHAR(32),
-  email VARCHAR(32),
-  number_phone VARCHAR(16),
   password VARCHAR(32),
+  number_phone VARCHAR(16),
+  email VARCHAR(32),
   sur_name VARCHAR(32),
   name VARCHAR(32),
+  date_of_birth DATE(),
+  place_of_birth VARCHAR(100),
+  gender VARCHAR(10),
+  ethenic_type VARCHAR(32),
   address VARCHAR(32),
-  birth VARCHAR(32),
+  role_id INT,
+
+  CONSTRAINT fk_user_id_role
+  FOREIGN KEY (role_id)
+  REFERENCES Role (role_id)
 
 );
 
-CREATE TABLE Students(
-  id INT PRIMARY KEY,
+CREATE TABLE Roles(
+  role_id INT PRIMARY KEY,
+  role_name VARCHAR(32),
+  permission_id VARCHAR(32),
+
+  CONSTRAINT fk_rl_id_permission
+  FOREIGN KEY (permission_id)
+  REFERENCES Permissions (permission_id)
+);
+
+CREATE TABLE Permissions(
+  permission_id INT PRIMARY KEY,
+  permission_name VARCHAR(32),
+  acctive VARCHAR(32),
+);
+
+
+CREATE TABLE StudentExams(
+  student_exam_id INT PRIMARY KEY,
+  paid bit NOT NULL DEFAULT (0),
   user_id INT,
+
+
+  CONSTRAINT fk_stdex_id_user
+  FOREIGN KEY (user_id)
+  REFERENCES Users (user_id)
 );
 
 CREATE TABLE Teachers(
